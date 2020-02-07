0bf909c9bc10e9a99c243b0823e6ad5555899c63
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-02-07 18:03:51 +0100

    Añadidas nuevas excepciones para la base de datos #6

--- src/exceptions.py
+++ src/exceptions.py
@@ -6,9 +6,31 @@ Class which contains the specific exceptions for the project.
 @author: Lidia Sánchez Mérida
 """
 class ProfileNotFound(Exception):
+    """Class exception to point out that there's not profile."""
     def __init__(self, mensaje):
         self.mensaje = mensaje
 
 class BasicProfileDataNotFound(Exception):
+    """Class exception to point out that the provided profile is not right."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+class CollectionNotFound(Exception):
+    """Class exception to point out that the current collection doesn't exist."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+class NewItemNotFound(Exception):
+    """Class exception to point out that the new item to insert doesn't exist."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+class EmptyCollection(Exception):
+    """Class exception to point out that the current collection is empty."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+    
+class ItemNotFound(Exception):
+    """Class exception to point out that the specific item doesn't exist."""
     def __init__(self, mensaje):
         self.mensaje = mensaje
