149ac10d28d3c3589003ffff60077a6fd0182e5b
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-02-19 23:48:08 +0100

    Nuevas excepciones para la recogida de datos #6

--- src/exceptions.py
+++ src/exceptions.py
@@ -34,3 +34,45 @@ class ItemNotFound(Exception):
     """Class exception to point out that the specific item doesn't exist."""
     def __init__(self, mensaje):
         self.mensaje = mensaje
+        
+class SingletonClass(Exception):
+    """Class exception to point out that the class API is a singleton class."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+class InvalidCredentials(Exception):
+    """Class exception to point out that the provided credentials are wrong."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+class MaxRequestsExceed(Exception):
+    """Class exception to point out that the maximum requests to the API has been
+        exceed. You'll have to wait some time to do more."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+class UsernameNotFound(Exception):
+    """Class exception to point out that the username specified doesn't exist."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+class RelathionshipsListNotFound(Exception):
+    """Class exception to point out that the lists of followers and followings 
+        don't exist."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+class LikersDictNotFound(Exception):
+    """Class exception to point out that likers should be a dict."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+class IdNotFound(Exception):
+    """Class exception to point out that a record doesn't have an id."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+        
+class PostsDictNotFound(Exception):
+    """Class exception to point out that a record doesn't have an id."""
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
