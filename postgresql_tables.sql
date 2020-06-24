12be788717a639cbf007d203ae77aa116fc507a9
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-06-24 11:50:25 +0200

    Crear la tabla profiles #13

---
+++ postgresql_tables.sql
@@ -0,0 +1,23 @@
+--
+-- PostgreSQL database dump
+--
+CREATE TABLE public.profile (
+    username character varying(20) NOT NULL,
+    date character varying(20) NOT NULL,
+    name character varying(100),
+    userid bigint NOT NULL,
+    biography character varying(200),
+    gender character varying(20),
+    profile_pic character varying(400),
+    location character varying(100),
+    birthday character varying(30),
+    date_joined character varying(20),
+    n_followers integer NOT NULL,
+    n_followings integer NOT NULL,
+    n_medias integer NOT NULL
+);
+--
+-- Name: profile profile_pkey; Type: CONSTRAINT; Schema: public;
+--
+ALTER TABLE ONLY public.profile
+    ADD CONSTRAINT profile_pkey PRIMARY KEY (username, date);
