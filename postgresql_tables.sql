2f56157628948f171e9afb16393d3ae51128ff4e
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-06-25 21:12:21 +0200

    Creadas el resto de tablas PostgreSQL #13

--- postgresql_tables.sql
+++ postgresql_tables.sql
@@ -1,7 +1,9 @@
 --
--- PostgreSQL database dump
+-- PostgreSQL database: socialnetworksdb.
 --
-CREATE TABLE public.profile (
+-- Table profiles which stores user personal data related to their social media accounts.
+--
+CREATE TABLE public.profiles (
     username character varying(20) NOT NULL,
     date character varying(20) NOT NULL,
     name character varying(100),
@@ -17,10 +19,98 @@ CREATE TABLE public.profile (
     n_medias integer NOT NULL
 );
 
-ALTER TABLE public.profile OWNER TO lidia;
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.profiles OWNER TO lidia;
 
 --
--- Name: profile profile_pkey; Type: CONSTRAINT; Schema: public;
+-- The Primary Key is made up of two fields: the username and the date in which
+--  the row has been inserted. In this way, multiple data of a same user can
+--  be stored in order to keep many records of them.
 --
-ALTER TABLE ONLY public.profile
+ALTER TABLE ONLY public.profiles
     ADD CONSTRAINT profile_pkey PRIMARY KEY (username, date);
+    
+--
+-- Table followers, in which the usernames of the users who are followed by an
+--  user are stored. Its Primary Key is an auto-increment id number.
+--
+CREATE TABLE followers(
+    id_follower SERIAL PRIMARY KEY, 
+    id_profile VARCHAR(20) NOT NULL, 
+    username VARCHAR(20) NOT NULL
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.followers OWNER TO lidia;
+
+--
+-- Table followings, in which the usernames of the followers of an user are stored.
+--  Its Primary Key is an auto-increment id number.
+--
+CREATE TABLE followings(
+    id_following SERIAL PRIMARY KEY, 
+    id_profile VARCHAR(20) NOT NULL, 
+    username VARCHAR(20) NOT NULL
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.followings OWNER TO lidia;
+
+--
+-- Table posts in which the media items of the users are stored. It contains
+--  fields such as the id of the post, the username who is the owner of the 
+--  post, the number of likes and comments about it.
+--
+-- The Primary Key is the id media returned by the API.
+--
+CREATE TABLE posts(
+    id_media VARCHAR(100) PRIMARY KEY, 
+    username VARCHAR(20) NOT NULL, 
+    like_count INTEGER NOT NULL CHECK (like_count>=0), 
+    com_count INTEGER NOT NULL CHECK (com_count>=0)
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.posts OWNER TO lidia;
+
+--
+-- Table likers in which the users who liked a post of the another user are stored
+--  along with the number of posts liked. 
+--
+CREATE TABLE likers(
+    id_liker SERIAL PRIMARY KEY, 
+    id_profile VARCHAR(20) NOT NULL, 
+    username VARCHAR(20) NOT NULL, 
+    n_likes INTEGER NOT NULL CHECK (n_likes>=0)
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.likers OWNER TO lidia;
+
+--
+-- Table comments in which the post comments are stored. It contains fields
+--  such as the text of the comment, the user who wrote it, the post in which
+--  has been written (foreign key) and the user who is the owner of the post.
+--
+CREATE TABLE comments(
+    id_comment SERIAL PRIMARY KEY, 
+    id_profile VARCHAR(20) NOT NULL, 
+    id_media VARCHAR(100) NOT NULL REFERENCES posts(id_media), 
+    username VARCHAR(20) NOT NULL, 
+    text VARCHAR(500) NOT NULL
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.comments OWNER TO lidia;
