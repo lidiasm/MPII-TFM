a17edc07f6c66cf170341a245d102c25416d5b76
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-06-26 12:39:22 +0200

    Separados los procesos de descarga y preprocesamiento-almacenamiento de datos #12

--- src/main_ops.py
+++ src/main_ops.py
@@ -11,7 +11,8 @@ sys.path.append('src/data')
 from api import Api
 import commondata
 from mongodb import MongoDB
-from exceptions import UsernameNotFound, MaxRequestsExceed
+from exceptions import UsernameNotFound, MaxRequestsExceed, UserDataNotFound \
+    , InvalidSocialMediaSource
 
 class MainOperations:
     
@@ -19,44 +20,55 @@ class MainOperations:
         """Constructor. It has a MongoDB object to work with the database."""
         self.mongodb = MongoDB(os.environ.get("MONGODB_URI"), 'SocialNetworksDB', 'profiles')
     
-    def get_user_instagram_data(self, search_user):
+    def get_user_instagram_common_data(self, search_user):
         """Downloads Instagram data of a specific user using the LevPasha Instagram
             API and stores the main fields in a MongoDB database."""
         if (search_user == None or type(search_user) != str or search_user == ""):
             raise UsernameNotFound("ERROR. Invalid username.")
             
-        result = {'profile':None, 'posts':None, 'contacts': None}
         inst_api = Api()
         try:
             """Download Instagram user data"""
             user_instagram_data = inst_api.get_levpasha_instagram_data(search_user)
-            """Preprocess data"""
-            cd = commondata.CommonData(self.mongodb, user_instagram_data)
-            prep_user_data = cd.preprocess_user_data()        
-            """Store preprocessed user profile to MongoDB collection 'profiles'"""
-            profile = cd.add_user_data(prep_user_data['profile']['username'],
-               prep_user_data['profile'], 'profiles', 'Instagram')            
-            
-            """Store user posts, their likers and comments into the same MongoDB collection 'posts'"""
-            all_posts = {}
-            all_posts['posts'] = prep_user_data['posts']
-            all_posts['likers'] = prep_user_data['likers']
-            all_posts['comments'] = prep_user_data['comments']
-            posts = cd.add_user_data(prep_user_data['profile']['username'],
-                 all_posts, 'posts', 'Instagram')
-            """Store the user followings and followers into the same MongoDB collection 'contacts'."""
-            all_contacts = {}
-            all_contacts['followings'] = prep_user_data['followings']
-            all_contacts['followers'] = prep_user_data['followers']
-            contacts = cd.add_user_data(prep_user_data['profile']['username'], 
-                all_contacts, 'contacts', 'Instagram')
-            
-            """Returns the final user data"""
-            result['profile'] = profile[1]
-            result['contacts'] = contacts[1]
-            result['posts'] = posts[1]
-            
-            return result
+            """Preprocess and store user data"""
+            user_data = self.preprocess_and_store_common_data(user_instagram_data, 'Instagram')
+            return user_data
         
         except MaxRequestsExceed:
             raise MaxRequestsExceed("Max requests exceed. Wait to send more.")
+    
+    def preprocess_and_store_common_data(self, user_data, social_media):
+        """Preprocess and store user data from any API source into the MongoDB database."""
+        if (type(user_data) != dict or len(user_data) == 0):
+            raise UserDataNotFound("ERROR. User data should be a non empty dict.")
+        if (type(social_media) != str or social_media == ""):
+            raise InvalidSocialMediaSource("ERROR. Social media type should be a non empty string.")
+        
+        # Preprocess data
+        cd = commondata.CommonData(self.mongodb, user_data)
+        prep_user_data = cd.preprocess_user_data()        
+        """Store preprocessed user profile to MongoDB collection 'profiles'"""
+        profile = cd.add_user_data(prep_user_data['profile']['username'],
+           prep_user_data['profile'], 'profiles', social_media)            
+        
+        """Store user posts, their likers and comments into the same MongoDB collection 'posts'"""
+        all_posts = {}
+        all_posts['posts'] = prep_user_data['posts']
+        all_posts['likers'] = prep_user_data['likers']
+        all_posts['comments'] = prep_user_data['comments']
+        posts = cd.add_user_data(prep_user_data['profile']['username'],
+             all_posts, 'posts', social_media)
+        """Store the user followings and followers into the same MongoDB collection 'contacts'."""
+        all_contacts = {}
+        all_contacts['followings'] = prep_user_data['followings']
+        all_contacts['followers'] = prep_user_data['followers']
+        contacts = cd.add_user_data(prep_user_data['profile']['username'], 
+            all_contacts, 'contacts', social_media)
+        
+        """Return the final user data"""
+        result = {'profile':None, 'posts':None, 'contacts': None}
+        result['profile'] = profile[1]
+        result['contacts'] = contacts[1]
+        result['posts'] = posts[1]
+        
+        return result
--- tests/test_main_ops.py
+++ tests/test_main_ops.py
@@ -9,21 +9,50 @@ import sys
 import pytest
 sys.path.append('src')
 import main_ops 
-from exceptions import UsernameNotFound, MaxRequestsExceed
+from exceptions import UsernameNotFound, MaxRequestsExceed, UserDataNotFound \
+    , InvalidSocialMediaSource
 
-def test1_get_user_instagram_data():
+def test1_preprocess_and_store_common_data():
+    """Test to check the method which preprocesses and stores user data without
+        providing the user data."""
+    mo = main_ops.MainOperations()
+    with pytest.raises(UserDataNotFound):
+        assert mo.preprocess_and_store_common_data(None, None)
+        
+def test2_preprocess_and_store_common_data():
+    """Test to check the method which preprocesses and stores user data without
+        providing the social media source"""
+    mo = main_ops.MainOperations()
+    with pytest.raises(InvalidSocialMediaSource):
+        assert mo.preprocess_and_store_common_data({'username':'lidia'}, None)
+
+def test3_preprocess_and_store_common_data():
+    profile = {'username':'Lidia'}
+    posts = {'123654': {'likes': '367', 'comments': '12'}}
+    comments = {'123':[{'user': 'ana', 'comment': 'Hey hey!'}, {'user': 'eva', 'comment': 'So cool!'}]}
+    likers = [('ana',84), ('maria',54)]
+    followers = ['anaortiz', 'luciav']
+    followings = ['anaortiz', 'luciav']
+    user_data = {'profile':profile, 'posts':posts, 'likers':likers, 
+                 'comments':comments, 'followers':followers, 'followings':followings}
+            
+    mo = main_ops.MainOperations()
+    result = mo.preprocess_and_store_common_data(user_data, "Instagram")
+    assert type(result) == dict
+    
+def test1_get_user_instagram_common_data():
     """Test to check if the method can get, preprocess and store an Instagram user data."""
     try:
         mo = main_ops.MainOperations()
         username = "lidia.96.sm"
-        result = mo.get_user_instagram_data(username)
+        result = mo.get_user_instagram_common_data(username)
         assert result['profile'] != None and result['contacts'] != None and result['posts'] != None
     except MaxRequestsExceed:
         print("Max requests exceed. Wait to send more.")
 
-def test2_get_user_instagram_data():
+def test2_get_user_instagram_common_data():
     """Test to check if the method can get, preprocess and store an Instagram user
         data without specifing the username."""
     mo = main_ops.MainOperations()
     with pytest.raises(UsernameNotFound):
-        assert mo.get_user_instagram_data('')
+        assert mo.get_user_instagram_common_data('')
