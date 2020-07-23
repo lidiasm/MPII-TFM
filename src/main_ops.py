657ee32fc96718b5bc2dea9942f36df149af104d
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-07-23 18:54:26 +0200

    Cambiada la inicialización de los objetos de bases de datos #12

--- src/main_ops.py
+++ src/main_ops.py
@@ -11,15 +11,34 @@ sys.path.append('src/data')
 sys.path.append('data')
 from api import Api
 import commondata
+import data_analyzer
 from mongodb import MongoDB
+from postgresql import PostgreSQL
 from exceptions import UsernameNotFound, MaxRequestsExceed, UserDataNotFound \
-    , InvalidSocialMediaSource
+    , InvalidSocialMediaSource, InvalidDatabaseCredentials, InvalidMongoDbObject \
+    , InvalidAnalysis, InvalidPreferences
 
 class MainOperations:
 
     def __init__(self):
-        """Constructor. It has a MongoDB object to work with the database."""
-        self.mongodb = MongoDB(os.environ.get("MONGODB_URI"), 'SocialNetworksDB', 'profiles')
+        """Constructor. It creates a MainOperations objects whose attributes are:
+            - CommonData object to preprocess the common data.
+            - MongoDB object to work with the first database.
+            - DataAnalyzer object to perform the different avalaible analysis.
+            - A list with the different avalaible analysis to perform.
+            - PostgreSQL object to insert and get data from the SQL tables.
+        """
+        psql_user = os.environ.get("POSTGRES_USER")
+        psql_pswd = os.environ.get("POSTGRES_PSWD")
+        if (type(psql_user) != str or type(psql_pswd) != str or psql_user == "" or psql_pswd == ""):
+            raise InvalidDatabaseCredentials("ERROR. PostgreSQL should be non-empty strings.")
+            
+        self.mongodb = MongoDB('profiles')
+        self.common_data = commondata.CommonData(self.mongodb)
+        self.data_analysis = data_analyzer.DataAnalyzer()
+        self.avalaible_analysis = ["ProfileEvolution", "SortPosts", "PostsEvolution",
+                         "FollowersActivity", "ContactsActivity", "GeneralBehaviour", "Haters/Friends"]
+        self.postgresql = PostgreSQL()
 
     def get_user_instagram_common_data(self, search_user):
         """Downloads Instagram data of a specific user using the LevPasha Instagram
@@ -29,7 +48,7 @@ class MainOperations:
         try:
             """Connect to the Levpasha Instagram API"""
             inst_api = Api()
-            inst_api.connect_levpasha_instagram_api(use_session_file=False)
+            inst_api.connect_levpasha_instagram_api()
             """Download Instagram user data"""
             user_instagram_data = inst_api.get_levpasha_instagram_data(search_user)
             """Preprocess and store user data"""
@@ -39,12 +58,15 @@ class MainOperations:
             raise MaxRequestsExceed("Max requests exceed. Wait to send more.")
 
     def preprocess_and_store_common_data(self, user_data, social_media):
-        """Preprocess and store user data from any API source into the MongoDB database."""
+        """Preprocesses and stores user data from any API source into the MongoDB database."""
         if (type(user_data) != dict or len(user_data) == 0):
             raise UserDataNotFound("ERROR. User data should be a non empty dict.")
         if (type(social_media) != str or social_media == ""):
             raise InvalidSocialMediaSource("ERROR. Social media type should be a non empty string.")
-
+        if (type(self.mongodb) != MongoDB):
+            raise InvalidMongoDbObject("ERROR. The connection to the MongoDB database "+
+                                       "should be a MongoDB object.")
+            
         # Preprocess data
         cd = commondata.CommonData(self.mongodb)
         prep_user_data = cd.preprocess_user_data(user_data)
@@ -73,3 +95,17 @@ class MainOperations:
         result['posts'] = posts[1]
 
         return result
+    
+    # def perform_analysis(self, data_analysis):
+    #     """Method which searchs for the required data to the Mongo database in 
+    #         order to preprocess and store them in the PostgreSQL database. Then,
+    #         depending on the type of analysis and specified preferences, it gets
+    #         the required data from PostgreSQL in order to perform the specified analysis.
+    #     """
+    ######## VER SI ES UN DICT
+    ######## CAMPOS OBLIGATORIOS Y NO VACÍOS: user, analysis
+    ##### OBTENER LOS DATOS DE MONGODB EN FUNCIÓN DEL ANÁLISIS ELEGIDO
+    ##### FILTRARLOS POR LAS FECHAS QUE SEAN PROPORCIONANDOLAS PARA QUE EL USUARIO ESCOJA SOBRE SEGURO
+    ##### VER LAS PREFERENCIAS EN FUNCIÓN DEL ANÁLISIS QUE SEA Y PONER LAS EXCEPCIONES
+    ###### PARÁMETROS DEL DICT: {'user':<username>, 'init_date':<str or None>, 'fin_date':<str or None>,
+    ######                          'analysis':<str>, 'preferences':{}}
--- tests/test_main_ops.py
+++ tests/test_main_ops.py
@@ -5,15 +5,33 @@ Tests to check the methods of the class MainOperations.
 
 @author: Lidia Sánchez Mérida
 """
+import os
 import sys
 import pytest
 sys.path.append('src')
 import main_ops 
 from exceptions import UsernameNotFound, MaxRequestsExceed, UserDataNotFound \
-    , InvalidSocialMediaSource
-
+    , InvalidSocialMediaSource, InvalidDatabaseCredentials
+        
+def test1_constructor():
+    """Test to check the constructor when PostgreSQL credentials are not provided to
+        connect to the PostgreSQL database. An exception will be raised."""
+    global psql_user
+    psql_user = os.environ["POSTGRES_USER"]
+    os.environ["POSTGRES_USER"] = ""
+    global psql_pswd
+    psql_pswd = os.environ.get("POSTGRES_PSWD")
+    os.environ["POSTGRES_PSWD"] = ""
+    with pytest.raises(InvalidDatabaseCredentials):
+        assert main_ops.MainOperations()
+    
 def test1_get_user_instagram_common_data():
-    """Test to check if the method can get, preprocess and store an Instagram user data."""
+    """Test to check if the method can get, preprocess and store an Instagram user data.
+        The PostgreSQL credentials are set again."""
+    global psql_user
+    os.environ["POSTGRES_USER"] = psql_user
+    global psql_pswd
+    os.environ["POSTGRES_PSWD"] = psql_pswd
     try:
         mo = main_ops.MainOperations()
         username = "lidia.96.sm"
@@ -45,10 +63,10 @@ def test2_preprocess_and_store_common_data():
 
 def test3_preprocess_and_store_common_data():
     profile = {"userid" : 123456789, "name" : "Lidia Sánchez", "username" : "lidia.96.sm", 
-               "biography" : "\"Si eres valiente para empezar, eres fuerte para acabar.\" Ingeniería Informática.",
-               "gender" : "None", "profile_pic" : "https://instagram_example", 
-               "location" : "None", "birthday" : "None", "date_joined" : "None", 
-               "n_followers" : 61, "n_medias" : 6, "id" : "lidia.96.sm", "date" : "13-07-2020", "social_media" : "Instagram" }
+                "biography" : "\"Si eres valiente para empezar, eres fuerte para acabar.\" Ingeniería Informática.",
+                "gender" : "None", "profile_pic" : "https://instagram_example", 
+                "location" : "None", "birthday" : "None", "date_joined" : "None", 
+                "n_followers" : 61, "n_medias" : 6, "id" : "lidia.96.sm", "date" : "13-07-2020", "social_media" : "Instagram" }
 
     posts = [{'id_post': '1', 'likes': 29, 'comments': 14}, 
               {'id_post': '2', 'likes': 18, 'comments': 0}]
