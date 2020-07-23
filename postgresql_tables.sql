42cdbfd8e54c235cd434ca322e2fbab1e587e691
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-07-23 18:55:40 +0200

    Esquema corregido y provisional de PostgreSQL #13

--- postgresql_tables.sql
+++ postgresql_tables.sql
@@ -2,6 +2,8 @@
 -- PostgreSQL database: socialnetworksdb.
 --
 -- Table profiles which stores user personal data related to their social media accounts.
+-- The field social_media indicates the social media source which the user profile came from.
+-- In this way, we can analyze if the users have different behaviours in different social medias.
 --
 CREATE TABLE public.profiles (
     username VARCHAR(20) NOT NULL,
@@ -16,7 +18,7 @@ CREATE TABLE public.profiles (
     date_joined VARCHAR(20),
     n_followers INTEGER NOT NULL,
     n_followings INTEGER NOT NULL,
-    n_medias INTEGER NOT NULL,
+    n_posts INTEGER NOT NULL,
     social_media VARCHAR(20) NOT NULL
 );
 
@@ -27,57 +29,47 @@ ALTER TABLE public.profiles OWNER TO lidia;
 
 --
 -- The Primary Key is made up of two fields: the username and the date in which
---  the row has been inserted. In this way, multiple data of a same user can
---  be stored in order to keep many records of them.
+--  the data has been inserted. In this way, multiple data of a same user can
+--  be stored in order to keep several records of them.
 --
 ALTER TABLE ONLY public.profiles
     ADD CONSTRAINT profile_pkey PRIMARY KEY (username, date);
     
 --
--- Table followers, in which the usernames of the users who are followed by an
---  user are stored. Its Primary Key is an auto-increment id number.
+-- Table contacts which stores the followers and followings of an user. In order
+-- to determinate wether the contact user is follower, following or both there is
+-- a field called relationship which could be one of the three previous values (follower, following, both).
 --
-CREATE TABLE followers(
-    id_follower SERIAL PRIMARY KEY, 
-    id_profile VARCHAR(20) NOT NULL, 
+-- This table also has a foreign key to the profiles table in order to relate the
+-- the contacts with the user who follow, is following or bot.
+--
+CREATE TABLE contacts(
+    id_contact SERIAL PRIMARY KEY, 
     username VARCHAR(20) NOT NULL,
-    social_media VARCHAR(20) NOT NULL
+    date VARCHAR(20) NOT NULL,
+    contact_username VARCHAR(20) NOT NULL, 
+    relationship VARCHAR(20) NOT NULL,
+    social_media VARCHAR(20) NOT NULL,
+    FOREIGN KEY (username, date) REFERENCES profiles(username,date)
 );
-
+    
 -- 
 -- Assign an owner to the table in order to operate with it.
 --
-ALTER TABLE public.followers OWNER TO lidia;
+ALTER TABLE public.contacts OWNER TO lidia;
 
 --
--- Table followings, in which the usernames of the followers of an user are stored.
---  Its Primary Key is an auto-increment id number.
---
-CREATE TABLE followings(
-    id_following SERIAL PRIMARY KEY, 
-    id_profile VARCHAR(20) NOT NULL, 
-    username VARCHAR(20) NOT NULL,
-    social_media VARCHAR(20) NOT NULL
-);
-
--- 
--- Assign an owner to the table in order to operate with it.
---
-ALTER TABLE public.followings OWNER TO lidia;
-
---
--- Table posts in which the media items of the users are stored. It contains
---  fields such as the id of the post, the username who is the owner of the 
---  post, the number of likes and comments about it.
---
--- The Primary Key is the id media returned by the API.
+-- Table posts which stores the main fields of each post: its id, the number of
+-- likes and the number of comments. 
 --
 CREATE TABLE posts(
-    id_media VARCHAR(100) PRIMARY KEY, 
-    username VARCHAR(20) NOT NULL, 
+    id_post VARCHAR(100) PRIMARY KEY, 
+    username VARCHAR(20) NOT NULL,
+    date VARCHAR(20) NOT NULL,
     like_count INTEGER NOT NULL CHECK (like_count>=0), 
     com_count INTEGER NOT NULL CHECK (com_count>=0),
-    social_media VARCHAR(20) NOT NULL
+    social_media VARCHAR(20) NOT NULL,
+    FOREIGN KEY (username, date) REFERENCES profiles(username,date)
 );
 
 -- 
@@ -86,37 +78,84 @@ CREATE TABLE posts(
 ALTER TABLE public.posts OWNER TO lidia;
 
 --
--- Table likers in which the users who liked a post of the another user are stored
---  along with the number of posts liked. 
+-- Table likers in which the users who liked posts are stored as well as the number
+-- of posts they liked. 
 --
 CREATE TABLE likers(
-    id_liker SERIAL PRIMARY KEY, 
-    id_profile VARCHAR(20) NOT NULL, 
-    username VARCHAR(20) NOT NULL, 
+    id_liker VARCHAR(100) PRIMARY KEY, 
+    username VARCHAR(20) NOT NULL,
+    date VARCHAR(20) NOT NULL,
+    liker_username VARCHAR(20) NOT NULL, 
     n_likes INTEGER NOT NULL CHECK (n_likes>=0),
-    social_media VARCHAR(20) NOT NULL
+    social_media VARCHAR(20) NOT NULL,
+    FOREIGN KEY (username, date) REFERENCES profiles(username,date)
 );
 
 -- 
 -- Assign an owner to the table in order to operate with it.
 --
 ALTER TABLE public.likers OWNER TO lidia;
-
---
--- Table comments in which the post comments are stored. It contains fields
---  such as the text of the comment, the user who wrote it, the post in which
---  has been written (foreign key) and the user who is the owner of the post.
+    
+    
+-- Table comments in which the post comments are stored. It contains fields like
+--  * The id of the related post. 
+--  * The username who wrote it.
+--  * The text of the comment.
 --
 CREATE TABLE comments(
-    id_comment SERIAL PRIMARY KEY, 
-    id_profile VARCHAR(20) NOT NULL, 
-    id_media VARCHAR(100) NOT NULL REFERENCES posts(id_media), 
-    username VARCHAR(20) NOT NULL, 
+    id_comment VARCHAR(100) PRIMARY KEY, 
+    username VARCHAR(20) NOT NULL,
+    date VARCHAR(20) NOT NULL,
+    id_post VARCHAR(100) NOT NULL,
+    comment_username VARCHAR(20) NOT NULL, 
     text VARCHAR(500) NOT NULL,
-    social_media VARCHAR(20) NOT NULL
+    social_media VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_post) REFERENCES posts(id_post),
+    FOREIGN KEY (username, date) REFERENCES profiles(username,date)
 );
 
 -- 
 -- Assign an owner to the table in order to operate with it.
 --
 ALTER TABLE public.comments OWNER TO lidia;
+
+-- Tables to perform the tests which check the behaviour of the Single Source
+-- of Truth which is the class PostgreSQL. Both of them are similar to 'profiles'
+-- and 'contacts' tables in order to reproduce the operations which will be done
+-- in the real tables.
+CREATE TABLE public.test1 (
+    username VARCHAR(20) NOT NULL,
+    date VARCHAR(20) NOT NULL,
+    name VARCHAR(100),
+    userid bigint NOT NULL,
+    biography VARCHAR(200),
+    gender VARCHAR(20),
+    profile_pic VARCHAR(400),
+    location VARCHAR(100),
+    birthday VARCHAR(30),
+    date_joined VARCHAR(20),
+    n_followers INTEGER NOT NULL,
+    n_followings INTEGER NOT NULL,
+    n_posts INTEGER NOT NULL,
+    social_media VARCHAR(20) NOT NULL
+);
+
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.test1 OWNER TO lidia;
+
+CREATE TABLE test2(
+    id_contact SERIAL PRIMARY KEY, 
+    username VARCHAR(20) NOT NULL,
+    date VARCHAR(20) NOT NULL,
+    contact_username VARCHAR(20) NOT NULL, 
+    relationship VARCHAR(20) NOT NULL,
+    social_media VARCHAR(20) NOT NULL,
+    FOREIGN KEY (username, date) REFERENCES profiles(username,date)
+);
+    
+-- 
+-- Assign an owner to the table in order to operate with it.
+--
+ALTER TABLE public.test2 OWNER TO lidia;
