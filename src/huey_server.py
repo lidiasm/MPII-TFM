d4b1d467897e58522d74a1476c8b29ebebcee10d
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2021-01-02 20:48:20 +0100

    Añadido servidor de tareas para descargar los datos de las redes sociales #23

---
+++ src/huey_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File which includes the automatic and periodic tasks to download data of a specific
+user and from a particular social media source.
+
+@author: Lidia Sánchez Mérida
+"""
+from huey import RedisHuey, crontab
+huey = RedisHuey('social_networks_app', host='localhost')
+
+from main_ops import MainOperations
+
+@huey.periodic_task(crontab(day='*/1', hour='19'))
+def get_user_data():
+    """
+    Function to download the data of a specific user from the selected social media
+    source every day at 7 o'clock.
+    """
+    mainops_object = MainOperations()
+    collected_data = {}
+    if (mainops_object.social_media_source.lower() == "instagram"):
+        collected_data = mainops_object.get_user_instagram_common_data(mainops_object.user_to_study, "real")
+
+    return collected_data
+
